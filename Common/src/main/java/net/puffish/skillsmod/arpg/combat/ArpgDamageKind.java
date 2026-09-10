ebbfcae8a572c745e6b786b96be7227f75be05ed
Gabalus <57585809+Gabalus@users.noreply.github.com>
2026-09-10 14:14:39 +0300

    Add ARPG damage source taxonomy

---
+++ Common/src/main/java/net/puffish/skillsmod/arpg/combat/ArpgDamageKind.java
@@ -0,0 +1,10 @@
+package net.puffish.skillsmod.arpg.combat;
+
+/** High-level ownership/source classification for ARPG damage and proc semantics. */
+public enum ArpgDamageKind {
+	BASIC_ATTACK,
+	ATTACK_SKILL,
+	SPELL,
+	DAMAGE_OVER_TIME,
+	TRIGGER
+}
